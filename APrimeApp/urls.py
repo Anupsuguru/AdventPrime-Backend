bd2a7728d27f768623f3373adaa1c8303b2f961c
Sriram Sridhar <sriramsridhar26@gmail.com>
2024-08-05 22:18:29 -0400

    minor changes between views and views_without_token_val

--- APrimeApp/urls.py
+++ APrimeApp/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('', index, name='index'),
     path('studentPreferences', student_preferences, name='studentPreferences'),
     path('categories', get_all_categories, name='get_all_categories'),
+    path('registeredworkshops',get_registered_workshops, name='get_registered_workshops'),
+    path('workshops/', get_all_workshops, name='get_all_workshops'),
+    path('threeworkshops/', get_three_workshops, name='get_three_workshops'),
+    path('register', register_for_workshop, name='register_for_workshop'),
+    path("cancelworkshop",cancelworkshop, name='cancelworkshop'),
+    path("attendance",attendance, name='attendance'),
 ]
--- APrimeApp/urls_without_token_val.py
+++ APrimeApp/urls_without_token_val.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views_without_token_val import *
 
 urlpatterns = [
-    path('', index, name='index'),
+    # path('', index, name='index'),
     path('studentPreferences', student_preferences, name='studentPreferences'),
     path('categories', get_all_categories, name='categories'),
     path('workshop/<str:workshop_id>/', get_workshop_details, name='get_workshop_details'),
